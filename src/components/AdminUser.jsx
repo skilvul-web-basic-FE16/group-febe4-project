0c8c5bda79fc1279588f1f6046f964c7c2b86bfd
FitriaaRainnnn <93276447+FitriaFajarAria@users.noreply.github.com>
2022-12-02 22:54:29 +0700

    Add files via upload

---
+++ src/components/AdminUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserList from '../pages/UserList'
+
+
+function AdminUser() {
+  return (
+    <div>
+      <UserList/>      
+    </div>
+  )
+}
+
+export default AdminUser
